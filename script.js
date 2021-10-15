1bcff8350b85f6b9c6e64507ec4d1dce7621e9e9
RanjithSurya <92427817+RanjithSurya@users.noreply.github.com>
2021-10-15 11:25:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/* $("#submitform").click(function(){
+    var fname = document.getElementById("inputFirstName").value();
+    var lname = document.getElementById("inputLastName").value();
+    var email = document.getElementById("inputEmail").value();
+    var phone = document.getElementById("inputPhoneNo").value();
+    var pincode = document.getElementById("inputPin").value();
+    var desc = document.getElementById("inputAddress").value();
+    console.log(fname);
+    console.log(lname);
+    console.log(email);
+    console.log(phone);
+    console.log(pincode);
+}); */
+/* 
+$( "#target" ).submit(function( event ) {
+    alert( "Handler for .submit() called." );
+    event.preventDefault();
+  });
+
+ */
+/* $(document).ready(function(){
+    $("#message").text("You can start filling the form");
+}); */
